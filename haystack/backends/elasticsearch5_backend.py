f9a8e88c873d7a0494c356528183ccdf13a8d7cf
Daniel Andrlik <daniel@andrlik.org>
2018-11-09 12:59:00 -0500

    Trying to fix issues with elasticsearch 5 changes

--- haystack/backends/elasticsearch5_backend.py
+++ haystack/backends/elasticsearch5_backend.py
@@ -15,7 +15,14 @@ from haystack.backends.elasticsearch_backend import (
 from haystack.constants import DEFAULT_OPERATOR, DJANGO_CT, FUZZINESS
 from haystack.exceptions import MissingDependency
 from haystack.utils import get_identifier, get_model_ct
-
+from haystack.constants import (
+    DEFAULT_OPERATOR,
+    DJANGO_CT,
+    DJANGO_ID,
+    FUZZY_MAX_EXPANSIONS,
+    FUZZY_MIN_SIM,
+    ID,
+)
 try:
     import elasticsearch
 
@@ -28,6 +35,19 @@ except ImportError:
                             installation of 'elasticsearch>=5.0.0,<6.0.0'. \
                             Please refer to the documentation."
     )
+
+DEFAULT_FIELD_MAPPING = {"type": "keyword", "analyzer": "snowball"}
+FIELD_MAPPINGS = {
+    "edge_ngram": {"type": "keyword", "analyzer": "edgengram_analyzer"},
+    "ngram": {"type": "keyword", "analyzer": "ngram_analyzer"},
+    "date": {"type": "date"},
+    "datetime": {"type": "date"},
+    "location": {"type": "geo_point"},
+    "boolean": {"type": "boolean"},
+    "float": {"type": "float"},
+    "long": {"type": "long"},
+    "integer": {"type": "long"},
+}
 
 
 class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
@@ -95,6 +115,43 @@ class Elasticsearch5SearchBackend(ElasticsearchSearchBackend):
                 self.log.error(
                     "Failed to clear Elasticsearch index: %s", e, exc_info=True
                 )
+
+    def build_schema(self, fields):
+        content_field_name = ""
+        mapping = {
+            DJANGO_ID: {
+                "type": "text",
+                "index": "not_analyzed",
+                "include_in_all": False,
+            },
+            DJANGO_CT: {
+                "type": "keyword",
+                "index": "not_analyzed",
+                "include_in_all": False,
+            },
+        }
+
+        for field_name, field_class in fields.items():
+            field_mapping = FIELD_MAPPINGS.get(
+                field_class.field_type, DEFAULT_FIELD_MAPPING
+            ).copy()
+            if field_class.boost != 1.0:
+                field_mapping["boost"] = field_class.boost
+
+            if field_class.document:
+                content_field_name = field_class.index_fieldname
+                field_mapping['type'] = 'text'
+
+            # Do this last to override `text` fields.
+            if field_mapping["type"] == "text":
+                if field_class.indexed is False or hasattr(field_class, "facet_for"):
+                    field_mapping["index"] = "not_analyzed"
+                    del field_mapping["analyzer"]
+
+            mapping[field_class.index_fieldname] = field_mapping
+
+        return (content_field_name, mapping)
+
 
     def build_search_kwargs(
         self,
